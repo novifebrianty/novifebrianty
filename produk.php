ee9f42416a5d87339fac710d27734cc6bb5a8063
novifebrianty <46737199+novifebrianty@users.noreply.github.com>
2019-01-16 12:17:41 +0700

    Add files via upload

--- produk.php
+++ produk.php
@@ -1,145 +1,42 @@
 <?php 
 
-Abstract class Produk {
-	private $judul,
-		   $penulis,
-		   $penerbit,
-		   $harga,
-		   $diskon = 0;
+class Produk {
+	public $judul = "judul",
+	       $penulis = "penulis",
+	       $penerbit = "penerbit",
+	       $harga = 0;
 
-		   
-
-public function __construct($judul = "judul", $penulis  = "penulis", $penerbit  ="penerbit", $harga  = 0){
-	$this->judul = $judul;
-	$this->penulis = $penulis;
-	$this->penerbit = $penerbit;
-	$this->harga = $harga;
-
-}
-
-public function setJudul ($judul) {
-	$this->judul = $judul;
-}
-public function getJudul(){
-	return $this->judul;
-}
-
-public function setPenulis ($penulis) {
-	$this->penulis = $penulis;
-}
-public function getPenulis(){
-	return $this->penulis;
-}
-
-public function setPenerbit ($penerbit) {
-	$this->penerbit = $penerbit;
-}
-public function getPenerbit(){
-	return $this->penerbit;
-}
-
-public function setDiskon( $diskon){
-	$this->diskon = $diskon;
-}
-public function getDiskon(){
-	return $this->diskon;
-}
-
-public function setHarga ($harga) {
-	$this->harga = $harga;
-}
-public function getHarga() {
-	return $this->harga - ( $this->harga * $this->diskon / 100);
-}
-
-public function getLabel(){
-	return "$this->penulis, $this->penerbit";
-}
-
-abstract public function getInfoProduk();
-
- public function getInfo() {
-	
-	$str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
-
-	return $str;
-}
-
-
-}
-
-class Komik extends Produk {
-	public $jmlHalaman;
-
-	public function __construct($judul = "judul", $penulis  = "penulis", $penerbit  ="penerbit", $harga  = 0, $jmlHalaman = 0) {
-
-		parent::__construct($judul, $penulis, $penerbit, $harga);
-
-		$this->jmlHalaman = $jmlHalaman;
-
-
-	}
+ public function getLabel(){
+     return "$this->penulis, $this->penerbit";
+ }
 
 
 
-	public function getInfoProduk(){
-		$str = "Komik : " .$this->getInfo(). "- {$this->jmlHalaman} Halaman.";
+ }
 
-		return $str;
-	}
-}
+ // $produk1 = new Produk();
+ // $produk1-> judul = "Naruto";
+ // var_dump($produk1);
 
-class Game extends Produk {
-	public $waktuMain;
+// $produk2 = new Produk();
+// $produk2-> judul = "Uncharted";
+// $produk2->tambahProperty = "novifebrianty";
+// var_dump($produk2);
 
-	public function __construct($judul = "judul", $penulis  = "penulis", $penerbit  ="penerbit", $harga  = 0, $waktuMain = 0) {
-		parent::__construct($judul, $penulis, $penerbit, $harga);
+ $produk3 = new Produk();
+ $produk3->judul = "Naruto";
+ $produk3->penulis = "Masashi Kishimoto";
+ $produk3->penerbit = "Shonen Jump";
+ $produk3->harga = 30000;
 
-		$this->waktuMain = $waktuMain;
+ $produk4 = new Produk();
+ $produk4->judul = "Uncharted";
+ $produk4->penulis = "Neil Druckman";
+ $produk4->penerbit = "Sonny Computer";
+ $produk4->harga = 250000;
 
-}
-
-
-	public function getInfoProduk() {
-		$str = "Game : " .$this->getInfo(). " - {$this->waktuMain} Jam.";
-
-		return $str;	
-	}
-}
-
-
-
-
-
-class CetakInfoProduk {
-	public $daftarProduk = array();
-
-	public function tambahProduk( Produk $produk ){
-	}
-
-
-	public function cetak(){
-		$str = "DAFTAR PRODUK : <br>";
-
-		foreach ($this->daftarProduk as $p) {
-			$str .= "- {$p->getInfoProduk()} <br>";
-		}
-		return $str;
-	}
-}
-
-
-
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
-
-$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000,50);
-
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk( $produk1 );
-$cetakProduk->tambahProduk( $produk2 );
-echo $cetakProduk->cetak();
-
-
-
+ echo "Komik : " . $produk3->getLabel();
+ echo "<br>";
+ echo "Game : " . $produk4->getLabel();
 
  ?>
